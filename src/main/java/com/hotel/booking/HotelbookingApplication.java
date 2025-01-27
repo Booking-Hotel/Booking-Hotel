78dc893f96546cdc4cded1336298344fa6da49c0
YoungYoon <duddbs1994@naver.com>
2025-01-27 17:36:54 +0900

    fix : HotelbookingApplication 파일 @EnableJpaAuditing 제거

--- src/main/java/com/hotel/booking/HotelbookingApplication.java
+++ src/main/java/com/hotel/booking/HotelbookingApplication.java
@@ -2,10 +2,8 @@ package com.hotel.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class HotelbookingApplication {
 
     public static void main(String[] args) {
