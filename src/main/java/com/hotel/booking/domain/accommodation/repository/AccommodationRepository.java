2175845597ebca083522cf145752eb9d74c7f0b7
youngseo3 <wys4475@gmail.com>
2025-02-04 23:56:55 +0900

    feat: LIKE 절을 사용하여 keyword로 검색하는 쿼리 작성 숙박 시설의 이름과 위치의 주소를 keyword로 검색한다.

--- src/main/java/com/hotel/booking/domain/accommodation/repository/AccommodationRepository.java
+++ src/main/java/com/hotel/booking/domain/accommodation/repository/AccommodationRepository.java
@@ -11,8 +11,15 @@ import java.util.List;
 @Repository
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
     @Query("SELECT new com.hotel.booking.domain.accommodation.dto.AccommodationListResponseDto(a, l) " +
-        "FROM Accommodation a " +
-        "JOIN Location l ON a.id = l.accommodation.id")
+            "FROM Accommodation a " +
+            "JOIN Location l ON a.id = l.accommodation.id")
     List<AccommodationListResponseDto> findAllWithLocation();
+
+    // 숙박 시설의 이름과 위치의 주소를 LIKE절로 keyword로 검색하는 쿼리
+    @Query("SELECT a FROM Accommodation a " +
+            "JOIN Location l ON a.id = l.accommodation.id " +
+            "WHERE a.id IN :accommodationIds " +
+            "AND (a.name LIKE %:keyword% OR l.address LIKE %:keyword%)")
+    List<Accommodation> findByIdsAndKeywordWithLocation(List<Long> accommodationIds, String keyword);
 
 }
